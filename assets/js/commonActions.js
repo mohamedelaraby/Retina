a407813b10a2db25630cf297f6dab3f8bcf2a8aa
Elias <elias@Eliass-iMac.local>
2020-08-25 17:12:51 +0200

    Toggle main section contianer status

--- assets/js/commonActions.js
+++ assets/js/commonActions.js
@@ -1,3 +1,55 @@
 $(document).ready(function (){
-    console.log("hi");
+    // Setup navbar click button
+    // if main section container has left padding class remove it else add it
+    $(".navShowHide").on('click',
+
+    /**
+     *  Toggle Main Section Container status
+     *  @return void
+     */
+    function (){
+        /**
+         *  @param  string main :- Hold main section container ID 
+         *  @param  string nav :- Hold Side nav container ID 
+         */
+        let main = ("#mainSectionContainer");
+        let nav = ("#sideNavContainer");
+
+        // Toggle main section container Status
+        if(main.hasClass("leftPadding")){
+            // Hide Side navigation
+            nav.hide();
+        } else {
+            // Show Side navigation
+            nav.show();
+        }
+
+        main.toggleClass("leftPadding");
+    });
 });
+
+
+
+    /**
+     *  Toggle Main Section Container status
+     *  @return void
+     */
+    function toggleMainSectionStatus(){
+        /**
+         *  @param  string main :- Hold main section container ID 
+         *  @param  string nav :- Hold Side nav container ID 
+         */
+        let main = ("#mainSectionContainer");
+        let nav = ("#sideNavContainer");
+
+        // Toggle main section container Status
+        if(main.hasClass("leftPadding")){
+            // Hide Side navigation
+            nav.hide();
+        } else {
+            // Show Side navigation
+            nav.show();
+        }
+
+        main.toggleClass("leftPadding");
+    }
