8f998fc443ca6d8fd509d6ab5ed54f18be7175fd
Elias <elias@Eliass-iMac.local>
2020-08-25 16:55:31 +0200

    Intial Release

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Retina - A video tube website</title>
+</head>
+    <body>
+    <!-- Start PageContainer -->
+        <div id="pageContainer">
+            <!-- Start MastHeadContainer -->
+            <div id="mastHeadContainer">
+                <button class="navShowHide">Click me!</button>
+
+            <!-- End MastHeadContainer -->
+            </div>
+
+            <!-- Start sideNavContainer -->
+            <div id="sideNavContainer">
+
+            <!-- End sideNavContainer -->
+            </div>
+
+            <!-- Start mainSectionContainer -->
+            <div id="mainSectionContainer">
+
+                <!-- Start mainContentContainer -->
+                <div id="mainContentContainer">
+                
+                <!-- End mainContentContainer -->
+                </div>
+
+                
+            <!-- End mainSectionContainer -->
+            </div>
+    <!-- End PageContainer -->
+        </div>
+
+
+
+    <!-- Start Libraries Scripts  -->
+    <script src="assets/js/libraries/jquery.min.js"></script>
+    <script src="assets/js/libraries/popper.min.js"></script>
+    <script src="assets/js/libraries/bootstrap.min.js"></script>
+    <!-- End Libraries Scripts  -->
+
+    <!-- Start Custom Scripts  -->
+    <script src="assets/js/commonActions.js"></script>
+    <script src="assets/js/main.js"></script>
+    <!-- End Custom Scripts  -->
+    </body>
+</html>
